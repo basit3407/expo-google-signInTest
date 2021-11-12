523374d649caa47586d3cfca813940f5b6f3b68d
Basit Minhas <basit3407@gmail.com>
2021-11-12 12:28:43 +0500

    ready-for-testing

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Button } from "react-native";
+import * as GoogleSignIn from "expo-google-sign-in";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const handlePress = async () => {
+    await GoogleSignIn.initAsync();
+
+    const { user } = await GoogleSignIn.signInAsync();
+    console.log("user in google = ", user); // ----> user.auth.token is null
+    const token = user?.auth?.idToken; //---->null
+    console.log("token in google = ", token);
+  };
+
+  return <Button title="Google SignIn" onPress={handlePress} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
